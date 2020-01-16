3acd830a1dd2b5cbecda5835ada3d251a3cb58a2
Gökay Umutlu <umutlugokay3535@gmail.com>
2020-01-16 22:29:51 +0300

    changed port back to 3306

--- db/db.js
+++ db/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool=mysql.createPool({
     host:"45.84.189.67",
     user:"gokayum1_adm",
-    port:"8080",
+    port:"3306",
     password:"461978_gs",
     database:"gokayum1_smartticket"
 });
