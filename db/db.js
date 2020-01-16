0a746e0ad99a70840bfd2ff72f6acec2ef69a965
Gökay Umutlu <umutlugokay3535@gmail.com>
2020-01-16 22:15:12 +0300

    changed port to 8080

--- db/db.js
+++ db/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool=mysql.createPool({
     host:"45.84.189.67",
     user:"gokayum1_adm",
-    port:"3306",
+    port:"8080",
     password:"461978_gs",
     database:"gokayum1_smartticket"
 });
